3792f39edf17f4a90109a76ef0e6f38636b7f816
Mueez Rizwan <156460813+MueezRiz@users.noreply.github.com>
2026-05-03 01:41:50 +0500

    Improve Tests

--- test_model.py
+++ test_model.py
@@ -1,7 +1,16 @@
 import joblib
 import numpy as np
 
+def test_model_exists():
+    model = joblib.load("models/model.pkl")
+    assert model is not None
+
 def test_prediction():
     model = joblib.load("models/model.pkl")
-    pred = model.predict(np.array([[5,6]]))
-    assert pred[0] in [0,1]
+    pred = model.predict(np.array([[5, 6]]))
+    assert pred[0] in [0, 1]
+
+if __name__ == "__main__":
+    test_model_exists()
+    test_prediction()
+    print("✅ All tests passed")
